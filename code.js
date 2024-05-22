ed070a8746a6dc78226ea618d0ee3443e3e8ad94
William the Buster <the5LMGVGOTY@hotmail.com>
2024-05-22 22:17:33 +0200

    It recognises the clicked cells!

--- code.js
+++ code.js
@@ -1,4 +1,15 @@
-function test() { console.log("Yay, HTML connects to this external JS file! "); }
-function click(e) {
-    console.log("Clicked cell at "+e.getBoundingClientRect().left+" "+e.getBoundingClientRect().top);
+function testConnection() {
+    console.log("Yay, HTML connects to this external JS file! ");
+    for (let cell of document.getElementsByClassName("cell")) cell.addEventListener('click', () => selectCell(cell));
+}
+let lastClickedCell=null;
+function selectCell(cell) {
+    if (lastClickedCell!=null) {
+        lastClickedCell.style.background=lastClickedCell.classList.contains("light")?"beige":"dimgray";
+    }
+    let classes=cell.classList;
+    let row= +classes[2].charAt(1), column=classes[3].charAt(1);
+    console.log("Clicked cell at "+column+row);
+    cell.style.background="green";
+    lastClickedCell=cell;
 }
