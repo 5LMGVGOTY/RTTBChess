e9e39487cefc7ab4b395716dc317ec19678359e4
William the Buster <the5LMGVGOTY@hotmail.com>
2024-05-27 00:25:12 +0200

    Added background, fixed colour schemes

--- code.js
+++ code.js
@@ -30,14 +30,14 @@ function selectCell(cell) {
         case '0': default: desc="nothing"; break;
     }
     console.log("Clicked cell at "+toCellNotation(row, column)+", there is "+desc+". ");
-    if (cell.style.background=="yellow") {
+    if (cell.style.background==="yellow") {
         move();
         for (let div of document.getElementsByClassName("cell")) {
-            div.style.background=div.classList.contains("light")?"beige":"dimgray";
+            div.style.background=div.classList.contains("light")?"burlywood":"saddlebrown";
         }
     } else {
         for (let div of document.getElementsByClassName("cell")) {
-            div.style.background=div.classList.contains("light")?"beige":"dimgray";
+            div.style.background=div.classList.contains("light")?"burlywood":"saddlebrown";
         }
         for (let moveOption of movementOptions(piece, row, column)) {
             for (let element of document.getElementsByClassName("r"+(moveOption[0]))) {
@@ -63,25 +63,25 @@ function movementOptions(piece, row, column) {
             if (row>1) cellsToMoveTo.push([row-1, column]);
             break;
         case 'Q': case 'q'://Queens
-            cellsToMoveTo=cellsToMoveTo.concat(movementOptions(piece==piece.toUpperCase()?'R':'r', row, column), 
-                                               movementOptions(piece==piece.toUpperCase()?'B':'b', row, column));
+            cellsToMoveTo=cellsToMoveTo.concat(movementOptions(piece===piece.toUpperCase()?'R':'r', row, column), 
+                                               movementOptions(piece===piece.toUpperCase()?'B':'b', row, column));
             break;
         case 'R': case 'r'://Rooks
             for (let c=row+1; c<=8; c++) {//up
                 cellsToMoveTo.push([c, column]);
-                if (interaction(piece, c, column)==BLOCKED) break;
+                if (interaction(piece, c, column)===BLOCKED) break;
             }
             for (let c=row-1; c>=1; c++) {//down
                 cellsToMoveTo.push([c, column]);
-                if (interaction(piece, c, column)==BLOCKED) break;
+                if (interaction(piece, c, column)===BLOCKED) break;
             }
             for (let c=column+1; c<=8; c++) {//right
                 cellsToMoveTo.push([row, c]);
-                if (interaction(piece, row, c)==BLOCKED) break;
+                if (interaction(piece, row, c)===BLOCKED) break;
             }
             for (let c=column-1; c>=1; c++) {//left
                 cellsToMoveTo.push([row, c]);
-                if (interaction(piece, row, c)==BLOCKED) break;
+                if (interaction(piece, row, c)===BLOCKED) break;
             }
             break;
         case 'N': case 'n'://Knights
@@ -94,39 +94,39 @@ function movementOptions(piece, row, column) {
             break;
         case 'B': case 'b'://Bishops
             for (let c=1; c<8; c++) {//up right
-                if (interaction(piece, row+c, column+c)==OUTSIDE) break;
+                if (interaction(piece, row+c, column+c)===OUTSIDE) break;
                 else {
                     cellsToMoveTo.push([row+c, column+c]);
-                    if (interaction(piece, row+c, column+c)==BLOCKED) break; 
+                    if (interaction(piece, row+c, column+c)===BLOCKED) break; 
                 }
             }
             for (let c=1; c<8; c++) {//down right
-                if (interaction(piece, row-c, column+c)==OUTSIDE) break;
+                if (interaction(piece, row-c, column+c)===OUTSIDE) break;
                 else {
                     cellsToMoveTo.push([row-c, column+c]);
-                    if (interaction(piece, row-c, column+c)==BLOCKED) break; 
+                    if (interaction(piece, row-c, column+c)===BLOCKED) break; 
                 }
             }
             for (let c=1; c<8; c++) {//down left
-                if (interaction(piece, row-c, column-c)==OUTSIDE) break;
+                if (interaction(piece, row-c, column-c)===OUTSIDE) break;
                 else {
                     cellsToMoveTo.push([row-c, column-c]);
-                    if (interaction(piece, row-c, column-c)==BLOCKED) break; 
+                    if (interaction(piece, row-c, column-c)===BLOCKED) break; 
                 }
             }
             for (let c=1; c<8; c++) {//up left
-                if (interaction(piece, row+c, column-c)==OUTSIDE) break;
+                if (interaction(piece, row+c, column-c)===OUTSIDE) break;
                 else {
                     cellsToMoveTo.push([row+c, column-c]);
-                    if (interaction(piece, row+c, column-c)==BLOCKED) break; 
+                    if (interaction(piece, row+c, column-c)===BLOCKED) break; 
                 }
             }
             break;
         case 'P': case 'p'://Pawns need fixin
-            let operation=((piece==piece.toUpperCase())?1:-1);
+            let operation=((piece===piece.toUpperCase())?1:-1);
             if (interaction(piece, row+operation, column)!=BLOCKED) {
                 cellsToMoveTo.push([row+operation, column]);
-                if (row==((piece==piece.toUpperCase())?2:7)&&interaction(piece, operation*2+row, column)!=BLOCKED) {
+                if (row===((piece===piece.toUpperCase())?2:7)&&interaction(piece, operation*2+row, column)!=BLOCKED) {
                     cellsToMoveTo.push([operation*2+row, column]);
                 }
             }
@@ -139,9 +139,9 @@ function movementOptions(piece, row, column) {
 }
 function interaction(piece, rowTarget, columnTarget) {
     var target=board[rowTarget][columnTarget];
-    if (target=='0') return EMPTY;
-    else if (target=='-') return OUTSIDE;
-    else if ((piece==piece.toUpperCase()&&target==target.toLowerCase())||(piece==piece.toUpperCase()&&target==target.toLowerCase())) return CAPTURE;
+    if (target==='0') return EMPTY;
+    else if (target==='-') return OUTSIDE;
+    else if ((piece===piece.toUpperCase()&&target===target.toLowerCase())||(piece===piece.toUpperCase()&&target===target.toLowerCase())) return CAPTURE;
     else return BLOCKED;
 }
 function move(columnStart, rowStart, columnEnd, rowEnd) {
