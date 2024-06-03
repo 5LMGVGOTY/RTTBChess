80ae02e2536dee5e8cb9bda042a0e2c838de758c
William the Buster <the5LMGVGOTY@hotmail.com>
2024-06-03 20:49:15 +0200

    Sike! Here are some animations and more beauty

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-var board=[
+var board=[//The data of where the pieces are
     ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'], //Row 1, light pieces
     ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'], 
     ['0', '0', '0', '0', '0', '0', '0', '0'], 
@@ -7,7 +7,7 @@ var board=[
     ['0', '0', '0', '0', '0', '0', '0', '0'], 
     ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'], 
     ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'], //Row 8, dark pieces
-], objects=[
+], objects=[//The img tags displayed on the board
     [null, null, null, null, null, null, null, null], 
     [null, null, null, null, null, null, null, null], 
     [null, null, null, null, null, null, null, null], 
@@ -31,11 +31,21 @@ const IMAGES=new Map([
     ['P', "Light_pawn"], 
     ['p', "Dark_pawn"]
 ]);
+var keypresses=["", "", "", "", "", "", "", "", "", ""];//do not think about this too much
 const EMPTY=0, CAPTURE=1, BLOCKED=2, LIGHT=1, DARK=2;
-var whatToShow=EMPTY;
+var whatToShow=EMPTY, shortCastleLight=true, longCastleLight=true, shortCastleDark=true, longCastleDark=true;
 const LIGHT_BUTTON=document.getElementsByTagName("button")[0], DARK_BUTTON=document.getElementsByTagName("button")[1];
 function init() {
-    console.log("Yay, HTML connects to this external JS file! ");
+    document.onkeydown=function(event) {//do not think about this too much
+        keypresses.push(event.key);
+        keypresses.shift();
+        if (keypresses[0]==="ArrowUp"&&keypresses[1]==="ArrowUp"&&keypresses[2]==="ArrowDown"&&keypresses[3]==="ArrowDown"&&keypresses[4]==="ArrowLeft"&&
+            keypresses[5]==="ArrowRight"&&keypresses[6]==="ArrowLeft"&&keypresses[7]==="ArrowRight"&&keypresses[8]==="b"&&keypresses[9]==="a") {
+            document.body.style.backgroundImage="url('Images/SecretBackground.webp')";
+            console.log("30 more lives for you! ");
+            document.onkeydown=null;
+        }
+    };
     for (let cell of document.getElementsByClassName("cell")) cell.addEventListener('click', () => selectCell(cell));
     LIGHT_BUTTON.addEventListener('click', () => inputMove(true));
     DARK_BUTTON.addEventListener('click', () => inputMove(false));
@@ -54,71 +64,70 @@ function init() {
     }
 }
 var columnStartLight, rowStartLight, columnEndLight, rowEndLight, columnStartDark, rowStartDark, columnEndDark, rowEndDark;
-function inputMove(isLight) {
+function inputMove(isLight) {//What happens when you press a button
     whatToShow=(isLight?LIGHT:DARK);
     LIGHT_BUTTON.setAttribute("disabled", "disabled");
     DARK_BUTTON.setAttribute("disabled", "disabled");
     if (isLight) LIGHT_BUTTON.innerText="Making light's move...";
     else DARK_BUTTON.innerText="Making dark's move...";
 }
-function resetCellColours() {
-     for (let lightCell of document.getElementsByClassName("light")) lightCell.style.background="turquoise";
-     for (let darkCell of document.getElementsByClassName("dark")) darkCell.style.background="brown";
+function resetCellAnims() {
+     for (let cell of document.getElementsByClassName("cell")) cell.style.animationName="none";
 }
-function selectCell(cell) {
+function selectCell(cell) {//What happens when you click on a cell
     let classes=cell.classList;
     let row= +classes[2].charAt(1), column= +classes[3].charAt(1);
     let piece=board[row][column];
-    let cellColour=cell.style.background;
-    resetCellColours();
-    if (cellColour==="yellow") {
-        resetCellColours();
+    let cellAnim=cell.style.animationName;
+    resetCellAnims();
+    if (cellAnim==="cellAnimYellow"||cellAnim==="cellAnimRed") {
+        resetCellAnims();
         if (whatToShow===LIGHT) {
             columnEndLight=column;
             rowEndLight=row;
-            resetCellColours();
+            resetCellAnims();
             LIGHT_BUTTON.innerText="Light's move made!";
             if (DARK_BUTTON.innerHTML==="Dark's move") DARK_BUTTON.removeAttribute("disabled");
             else moveCheck();
         } else if (whatToShow===DARK) {
             columnEndDark=column;
             rowEndDark=row;
-            resetCellColours();
+            resetCellAnims();
             DARK_BUTTON.innerText="Dark's move made!";
             if (LIGHT_BUTTON.innerHTML==="Light's move") LIGHT_BUTTON.removeAttribute("disabled");
             else moveCheck();
-        } else console.log("That wasn't supposed to happen :skull:");
+        } else console.warn("That wasn't supposed to happen :skull:");
         whatToShow=EMPTY;
     }
-    else if (cellColour!=="green") {
+    else if (cellAnim!=="cellAnimGreen") {
         switch(whatToShow) {
             case EMPTY:
-                cell.style.background="green";
+                cell.style.animationName="cellAnimGreen";
                 break;
             case LIGHT:
                 if (piece===piece.toUpperCase()) {
                     for (let moveOption of movementOptions(piece, row, column)) {
-                        document.getElementsByClassName("r"+moveOption[0]+" c"+moveOption[1])[0].style.background="yellow";
+                        document.getElementsByClassName("r"+moveOption[0]+" c"+moveOption[1])[0].style.animationName=board[moveOption[0]][moveOption[1]]==='0'?"cellAnimYellow":"cellAnimRed";
                     }
                     columnStartLight=column;
                     rowStartLight=row;
-                    cell.style.background="green";
+                    cell.style.animationName="cellAnimGreen";
                 } else alert("This is a piece of your opponent's! ");
                 break;
             case DARK:
                 if (piece===piece.toLowerCase()) {
                     for (let moveOption of movementOptions(piece, row, column)) {
-                        document.getElementsByClassName("r"+moveOption[0]+" c"+moveOption[1])[0].style.background="yellow";
+                        document.getElementsByClassName("r"+moveOption[0]+" c"+moveOption[1])[0].style.animationName="cellAnimYellow";
                     }
                     columnStartDark=column;
                     rowStartDark=row;
-                    cell.style.background="green";
+                    cell.style.animationName="cellAnimGreen";
                 } else alert("This is a piece of your opponent's! ");
                 break;
         }
     }
 }
-function movementOptions(piece, row, column) {
+function movementOptions(piece, row, column) {//What cells pieces can move to
     let cellsToMoveTo=[];
     switch(piece) {
         case 'K': case 'k'://Kings
@@ -206,16 +215,16 @@ function movementOptions(piece, row, column) {
     }
     return cellsToMoveTo;
 }
-function interaction(piece, target) {
+function interaction(piece, target) {//What would happen if piece would land on target
     if (target==='0') return EMPTY;
     else if ((piece===piece.toUpperCase()&&target===target.toLowerCase())||(piece===piece.toLowerCase()&&target===target.toUpperCase())) return CAPTURE;
     else return BLOCKED;
 }
-function moveCheck() {
+function moveCheck() {//Uses advanced logic and processing to determine the outcome of two simultaneous moves
     //gotta implement them all!
     move(true, true, true);
 }
-function move(lightPriority, legalLight, legalDark) {
+function move(lightPriority, legalLight, legalDark) {//Actually moves the pieces AND checks for collisions
     let imageLight=objects[rowStartLight][columnStartLight], imageDark=objects[rowStartDark][columnStartDark];
     let leftLight=columnStartLight*45, topLight=(7-rowStartLight)*45, columnDistanceLight=columnEndLight-columnStartLight, rowDistanceLight=rowEndLight-rowStartLight, 
         leftDark=columnStartDark*45, topDark=(7-rowStartDark)*45, columnDistanceDark=columnEndDark-columnStartDark, rowDistanceDark=rowEndDark-rowStartDark;
@@ -257,9 +266,8 @@ function move(lightPriority, legalLight, legalDark) {
         } else c++;
     }, 30);
 }
-function doDangerousStuffWithTheData(collision, lightPriority, legalLight, legalDark) {
+function doDangerousStuffWithTheData(collision, lightPriority, legalLight, legalDark) {//Changes the board and objects variables according to the moves' outcome
     if (collision) {
-        console.log("There was a collision");
         board[rowStartLight][columnStartLight]='0';
         board[rowStartDark][columnStartDark]='0';
         document.getElementById("board").removeChild(objects[rowStartLight][columnStartLight]);
@@ -267,19 +275,33 @@ function doDangerousStuffWithTheData(collision, lightPriority, legalLight, legal
         objects[rowStartLight][columnStartLight]=null;
         objects[rowStartDark][columnStartDark]=null;
     } else {
-        if (lightPriority) {
+        if (lightPriority && legalLight) {
+            if (interaction('K', board[rowStartLight][columnStartLight])===BLOCKED && rowStartLight===0) {
+                if (columnStartLight===0 || columnStartLight===4) longCastleLight=false;
+                if (columnStartLight===4 || columnStartLight===7) shortCastleLight=false;
+            }
             if (objects[rowEndLight][columnEndLight]!==null) document.getElementById("board").removeChild(objects[rowEndLight][columnEndLight]);
             board[rowEndLight][columnEndLight]=board[rowStartLight][columnStartLight];
             board[rowStartLight][columnStartLight]='0';
             objects[rowEndLight][columnEndLight]=objects[rowStartLight][columnStartLight];
             objects[rowStartLight][columnStartLight]=null;
         }
-        if (objects[rowEndDark][columnEndDark]!==null) document.getElementById("board").removeChild(objects[rowEndDark][columnEndDark]);
-        board[rowEndDark][columnEndDark]=board[rowStartDark][columnStartDark];
-        board[rowStartDark][columnStartDark]='0';
-        objects[rowEndDark][columnEndDark]=objects[rowStartDark][columnStartDark];
-        objects[rowStartDark][columnStartDark]=null;
-        if (!lightPriority) {
+        if (legalDark) {
+            if (interaction('K', board[rowStartDark][columnStartDark])===BLOCKED && rowStartDark===0) {
+                if (columnStartDark===0 || columnStartDark===4) longCastleDark=false;
+                if (columnStartDark===4 || columnStartDark===7) shortCastleDark=false;
+            }
+            if (objects[rowEndDark][columnEndDark]!==null) document.getElementById("board").removeChild(objects[rowEndDark][columnEndDark]);
+            board[rowEndDark][columnEndDark]=board[rowStartDark][columnStartDark];
+            board[rowStartDark][columnStartDark]='0';
+            objects[rowEndDark][columnEndDark]=objects[rowStartDark][columnStartDark];
+            objects[rowStartDark][columnStartDark]=null;
+        }
+        if (!lightPriority && legalLight) {
+            if (interaction('K', board[rowStartLight][columnStartLight])===BLOCKED && rowStartLight===0) {
+                if (columnStartLight===0 || columnStartLight===4) longCastleLight=false;
+                if (columnStartLight===4 || columnStartLight===7) shortCastleLight=false;
+            }
             if (objects[rowEndLight][columnEndLight]!==null) document.getElementById("board").removeChild(objects[rowEndLight][columnEndLight]);
             board[rowEndLight][columnEndLight]=board[rowStartLight][columnStartLight];
             board[rowStartLight][columnStartLight]='0';
